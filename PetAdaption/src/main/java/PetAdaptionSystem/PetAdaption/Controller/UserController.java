dd32177606f6d065cda8ee6571a38365b7a40325
Varun Prashanth V <v.varunprashanth@gmail.com>
2025-03-21 15:53:07 +0530

    working on user

--- PetAdaption/src/main/java/PetAdaptionSystem/PetAdaption/Controller/UserController.java
+++ PetAdaption/src/main/java/PetAdaptionSystem/PetAdaption/Controller/UserController.java
@@ -1,8 +1,52 @@
 package PetAdaptionSystem.PetAdaption.Controller;
 
-//import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import PetAdaptionSystem.PetAdaption.Entity.User;
+import PetAdaptionSystem.PetAdaption.Service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable int id) {
+        Optional<User> user = userService.getUserById(id);
+        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        return userService.addUser(user);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable int id, @RequestBody User updatedUser) {
+        Optional<User> user = userService.updateUser(id, updatedUser);
+        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable int id) {
+        Optional<User> user = userService.getUserById(id);
+        if (user.isPresent()) {
+            userService.deleteUser(id);
+            return ResponseEntity.ok(user.get()); // Returning deleted user data
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
---
+++ PetAdaption/src/main/java/PetAdaptionSystem/PetAdaption/Service/UserService.java
@@ -0,0 +1,48 @@
+package PetAdaptionSystem.PetAdaption.Service;
+
+import PetAdaptionSystem.PetAdaption.Entity.User;
+import PetAdaptionSystem.PetAdaption.Repository.UserRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(int id) {
+        return userRepository.findById(id);
+    }
+
+    public Optional<User> getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(int id) {
+        userRepository.deleteById(id);
+    }
+
+    public Optional<User> updateUser(int id, User updatedUser) {
+        return userRepository.findById(id).map(user -> {
+            user.setUserName(updatedUser.getUserName());
+            user.setAddress(updatedUser.getAddress());
+            user.setEmail(updatedUser.getEmail());
+            user.setPassword(updatedUser.getPassword());
+            user.setRole(updatedUser.getRole());
+            return userRepository.save(user);
+        });
+    }
+}
